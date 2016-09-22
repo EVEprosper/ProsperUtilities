96b6655fa342867773a23c4c06972dbe557db04e
Lockefox <locke.renard@gmail.com>
2016-09-21 19:49:40 -0700

    basic ConfigChecker functionality finished

--- scripts/ConfigChecker.py
+++ scripts/ConfigChecker.py
@@ -1,6 +1,7 @@
 '''ConfigChecker.py checks if local/tracked config files are equivallent'''
 
 import configparser
+from configparser import ExtendedInterpolation
 
 from plumbum import cli, local
 
@@ -12,7 +13,19 @@ class ConfigCheck(cli.Application):
     )
 
     base_file = ''
+    base_config = configparser.ConfigParser(
+        interpolation=ExtendedInterpolation(),
+        allow_no_value=True,
+        delimiters=('='),
+        inline_comment_prefixes=('#')
+    )
     comp_file = ''
+    comp_config = configparser.ConfigParser(
+        interpolation=ExtendedInterpolation(),
+        allow_no_value=True,
+        delimiters=('='),
+        inline_comment_prefixes=('#')
+    )
 
     @cli.switch(
         ['-b', '--base'],
@@ -23,6 +36,11 @@ class ConfigCheck(cli.Application):
         '''test and load base file for comparisons'''
         tmp_path = local.path(filename)
         if tmp_path.exists() and tmp_path.is_file():
+            try:
+                with open(tmp_path,'r') as config_handle:
+                    self.base_config.read_file(config_handle)
+            except Exception as error_msg:
+                raise error_msg
             self.base_file = tmp_path
         else:
             raise IOError('--base=' + filename + ' not found')
@@ -36,6 +54,11 @@ class ConfigCheck(cli.Application):
         '''test and load comp file for comparisons'''
         tmp_path = local.path(filename)
         if tmp_path.exists() and tmp_path.is_file():
+            try:
+                with open(tmp_path,'r') as config_handle:
+                    self.comp_config.read_file(config_handle)
+            except Exception as error_msg:
+                raise error_msg
             self.comp_file = tmp_path
         else:
             raise IOError('--comp=' + filename + ' not found')
@@ -53,6 +76,58 @@ class ConfigCheck(cli.Application):
             print('--base_file=' + self.base_file)
             print('--comp_file=' + self.comp_file)
 
+        section_list = []
+        for section in self.base_config:
+            section_list.append(section)
+            try:
+                self.comp_config[section]
+            except KeyError:
+                print('SECTION MISSING IN COMP: ' + section)
+                continue
+
+            for value in self.base_config[section]:
+                base_entry = self.base_config.get(section, value)
+                try:
+                    comp_entry=self.comp_config.get(section, value)
+                except KeyError:
+                    print('--KEY MISSING IN COMP: ' + value)
+                    continue
+
+                if base_entry:  #if key isn't blank, compare values
+                    if base_entry != comp_entry:
+                        print('--WARNING: base/comp entries different for {section}.{value}'.\
+                            format(
+                                section=section,
+                                value=value
+                            ))
+                        print('\tbase=' + base_entry)
+                        print('\tcomp=' + comp_entry)
+                    elif self.verbose:
+                        print('{section}.{value}==={entry}'.\
+                            format(
+                                section=section,
+                                value=value,
+                                entry=base_entry
+                            ))
+            for value in self.comp_config[section]:
+                comp_entry = self.comp_config.get(section, value)
+                try:
+                    base_entry = self.base_config.get(section, value)
+                except KeyError:
+                    print('--KEY MISSING IN BASE: ' + value)
+                    continue
+
+                #TODO: compare comp value differences?
+
+        for section in self.comp_config:
+            if section in section_list:
+                continue
+
+            try:
+                self.base_config[section]
+            except KeyError:
+                print('SECTION MISSING IN BASE: ' + section)
+                continue
 
 if __name__ == '__main__':
     ConfigCheck.run()
