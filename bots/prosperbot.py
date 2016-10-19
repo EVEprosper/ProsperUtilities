2d717585b0baadb45d846dfb5718b62052c76772
Lockefox <locke.renard@gmail.com>
2016-10-19 16:47:21 -0700

    added get_stock_data and get_company_name calls for fetching data

--- bots/prosperbot.py
+++ bots/prosperbot.py
@@ -2,6 +2,8 @@
 from os import path
 from datetime import datetime, timedelta
 
+#import requests
+import pandas as pd
 import pandas_datareader.data as web
 import discord
 from discord.ext import commands
@@ -15,8 +17,40 @@ CONFIG = get_config(CONFIG_ABSPATH)
 LOGGER = create_logger(
     'prosperbot',
     '.',
-    CONFIG
+    CONFIG,
+    'DEBUG'
 )
+
+TICKER_FORMAT = CONFIG.get('PD_DATAREADER', 'ticker_format')
+TICKER_LOOKUP = CONFIG.get('PD_DATAREADER', 'ticker_lookup')
+def get_company_name(ticker, ticker_format=TICKER_FORMAT):
+    ticker_url = '{base_url}?s={ticker}&f={ticker_format}'.format(
+        base_url=TICKER_LOOKUP,
+        ticker=ticker,
+        ticker_format=ticker_format
+    )
+    ticker_data = pd.read_csv(ticker_url)
+    return ticker_data.columns.values[0] #only fetch one company name
+
+QUOTE_SOURCE = CONFIG.get('PD_DATAREADER', 'quote_source')
+DATERANGE = CONFIG.get('PD_DATAREADER', 'DATERANGE')
+def get_stock_data(ticker, quote_source=QUOTE_SOURCE, daterange=DATERANGE):
+    start_date = datetime.today() - timedelta(days=int(daterange))
+    end_date = datetime.today()
+
+    try:
+        LOGGER.info('Fetching data for: ' + str(ticker))
+        LOGGER.debug(
+            '\r\tticker={0}'.format(ticker) +
+            '\r\tquote_source={0}'.format(ticker) +
+            '\r\tstart_date={0}'.format(start_date) +
+            '\r\tend_date={0}'.format(end_date)
+        )
+        stock_data = web.DataReader(ticker, quote_source, start_date, end_date)
+    except Exception as err_message:
+        raise err_message
+
+    return stock_data
 
 bot = commands.Bot(
     command_prefix=CONFIG.get('OAUTH', 'bot_prefix'),
@@ -31,7 +65,11 @@ async def on_ready():
     LOGGER.info('------')
 
 @bot.command(pass_context=True)
-async def quote(ctx):
+async def quote(ctx, symbol:str):
+    await bot.say(symbol)
+
+@bot.command(pass_context=True)
+async def echo(ctx):
     await bot.say('echo: {0}'.format(ctx.message.content))
 
 BOT_TOKEN = CONFIG.get('OAUTH', 'bot_token')
